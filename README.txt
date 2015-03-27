c260c06ff0a31358e5fbf48427ceb094ba32b911
danielgholmes <flyholmes@gmail.com>
2015-03-27 19:57:37 +0200

    Version 1 complete!

--- README.txt
+++ README.txt
@@ -1,14 +1,16 @@
-Demostrating Design by Contract - ATM withdrawal model
+Demonstrating Design by Contract - ATM withdrawal model
 
 Author: Daniel G. Holmes
 -----------------------------------------------------
 
-The purpose of the program is to model the withdrawal feature of two different types of ATM's in order demonstrate principles of Design by Contract. The one ATM is a standard ATM which has a limited set of bank notes available for withdrawal. This can also be though of as an ATM that has limited features. The other ATM can be thought of as an an advanved ATM, offering better services. Unlike the standard one, the advanced ATM offers all types of bank notes available in South Africa (200s, 100s, 50s, 20s, 10s).
+The purpose of this program is to model the withdrawal feature of two different types of ATM's in order to demonstrate principles of Design by Contract. The first ATM is a standard ATM which has a limited set of bank notes available for withdrawal (200's, 100's, 50's). This can also be thought of as an ATM that has limited features. The other ATM can be thought of as an an advanved ATM, offering better services. Unlike the standard one, the advanced ATM offers all types of bank notes available in South Africa (200's, 100's, 50's, 20's, 10's).
 
 The program consists of a parent class "ATM" and a derived class "AdvancedATM". The principles of Design by Contract are demonstrated within the context of these two classes. 
 
-Messages in the window encapsulated as "<< ... >>" are used to indicates when a specific point in the program has been reached. This aids in the demontration of the logical flow of a program that uses Design by Contract, especially where inheritance is involved.
+Messages in the window are encapsulated as "<< ... >>" and are used to indicate when a specific point in the program has been reached. This aids in the demonstration of the logical flow of a program that uses Design by Contract, especially where inheritance is involved.
 
 Example: "<<class.operation.suboperation>>"
 
 The suboperation specifies whether preconditions, postconditions or the function body has been run (each denoted by "in", "out" and "body" respectively).
+
+Any input to the program that 
--- source/atm.d
+++ source/atm.d
@@ -1,8 +1,8 @@
 class ATM
 {
 	invariant()
-	{
-		writeln("<<ATM.invariant>>");
+	{// should be maintained for this class and all those derived from it
+		writeln("<<ATM.invariant.check>>"); 
 	    assert(cashBalance == servicedAmount - withdrawsTotal);
 	}
 
@@ -11,7 +11,7 @@ class ATM
 	{
 		writeln("<<ATM.constructor.in>>");
 	    assert(twohundreds > 0 && hundreds > 0 && fifties > 0);
-		writeln("Preconditions: validation of contructor arguments ... OK!");
+		writeln("Preconditions: validation of constructor arguments ... OK!");
 	}
 	body
 	{
@@ -22,24 +22,52 @@ class ATM
 	    fiftyNotes= fifties;
 	    servicedAmount = twohundredNotes + hundredNotes + fiftyNotes;
 	    cashBalance = servicedAmount - withdrawsTotal;
+		withdrawLimit = 5000;
 	}
 
 	int[] withdraw(in int amount)
 	in
 	{
 		writeln("<<ATM.withdraw.in>>");
-		assert(amount > 0);
-		assert(amount % 50 == 0);
+		assert(amount > 0 && amount <= withdrawLimit);
+		assert(amount % 50 == 0); // preconditions that need to be met by any derived class
+		writeln("Preconditions: validation of withdraw function arguments ... OK!");
 	}
 	out (result)
 	{ // checks that only available notes are issued
 		writeln("<<ATM.withdraw.out>>");
 		assert(result[0] <= twohundredNotes && result[1] <= hundredNotes && result[2] <= fiftyNotes);
+		writeln("Postconditions: base class validation of function execution ... OK!");
 	}
 	body
 	{
 		writeln("<<ATM.withdraw.body>>");
-	    return [1, 2, 3];
+		int[] notesToIssue = [0,0,0];
+		int amountTracker = amount;
+
+		while(amountTracker != 0) // allocates notes to be issued
+		{
+			if (amountTracker >= 200)
+			{
+				amountTracker -= 200;
+				notesToIssue[0]++;
+				twohundredNotes--;
+			}	
+			else if (amountTracker >= 100)
+			{
+				amountTracker -= 100;
+				notesToIssue[1]++;
+				hundredNotes--;
+			}
+			else if (amountTracker >= 50)
+			{
+				amountTracker -= 50;
+				notesToIssue[2]++;
+				fiftyNotes--;
+			}
+			else break;
+		}
+	    return notesToIssue;
 	}
 
 protected:
@@ -51,6 +79,8 @@ protected:
 	int twohundredNotes;
 	int hundredNotes;
 	int fiftyNotes;
+
+	int withdrawLimit;
 
 	// other member functions ...
 }
@@ -64,7 +94,7 @@ public:
 	{
 		writeln("<<AdvancedATM.constructor.in>>");
 		assert(twohundreds > 0 && hundreds > 0 && fifties > 0 && twenties > 0 && tens > 0);
-		writeln("Preconditions: validation of contructor arguments ... OK!");
+		writeln("Preconditions: validation of constructor arguments ... OK!");
 	}
 	body
 	{
@@ -78,58 +108,153 @@ public:
 		tenNotes = tens;
 		servicedAmount = twohundredNotes + hundredNotes + fiftyNotes + twentyNotes + tenNotes;
 		cashBalance = servicedAmount - withdrawsTotal;
-		writeln("New advancedATM object successfully created!");
+		withdrawLimit = 5000;
+		displayAvailableNotes();
 	}
 
 	override int[] withdraw(in int amount)
 	in
 	{
+		writeln("Preconditions: validation of base class withdraw function arguments ... FAIL!");
 		writeln("<<AdvancedATM.withdraw.in>>");
-		assert(amount > 0);
+
+		assert(amount > 0 && amount <= withdrawLimit);
 		assert(amount % 50 == 0 || amount % 10 == 0); // weakens preconditions
+
+		writeln("Preconditions: validation of derived class withdraw function arguments ... OK!");
 	}
 	out (result)
 	{ 
 		writeln("<<AdvancedATM.withdraw.out>>");
+
 		assert(result[0] <= twohundredNotes && result[1] <= hundredNotes
 			&& result[2] <= fiftyNotes      && result[3] <= twentyNotes
 			&& result[4] <= tenNotes); // strengthens postconditions
+
+		writeln("Postconditions: derived class validation of function execution ... OK!");
+		writeln("Contracts have been kept. Class invariant maintained.");
+		writeln("You have been issues the following notes");
+		writeln("200's: ", result[0]);
+		writeln("100's: ", result[1]);
+		writeln("50's : ", result[2]);
+		writeln("20's : ", result[3]);
+		writeln("10's : ", result[4]);
+		writeln("CASH WITHDRAWAL SUCCESS!");
 	}
 	body
 	{
 		writeln("<<AdvancedATM.withdraw.body>>");
-		return [1, 2, 3, 4, 5];
+		int[] notesToIssue = [0,0,0,0,0];
+		int amountTracker = amount;
+
+		while(amountTracker != 0) // allocates notes to be issued
+		{
+			if (amountTracker >= 200)
+			{
+				amountTracker -= 200;
+				notesToIssue[0]++;
+				twohundredNotes--;
+			}	
+			else if (amountTracker >= 100)
+			{
+				amountTracker -= 100;
+				notesToIssue[1]++;
+				hundredNotes--;
+			}
+			else if (amountTracker >= 50)
+			{
+				amountTracker -= 50;
+				notesToIssue[2]++;
+				fiftyNotes--;
+			}
+			else if (amountTracker >= 20)
+			{
+				amountTracker -= 20;
+				notesToIssue[3]++;
+				twentyNotes--;
+			}
+			else if (amountTracker >= 10)
+			{
+				amountTracker -= 10;
+				notesToIssue[4]++;
+				tenNotes--;
+			}
+			else break;
+		}
+	    return notesToIssue;
 	}
 
 private:
 	int twentyNotes;
 	int tenNotes;
 
+	void displayAvailableNotes()
+	{
+		writeln("New advancedATM object successfully created!");
+		writeln("Available notes");
+		writeln("200's: ", twohundredNotes);
+		writeln("100's: ", hundredNotes);
+		writeln("50's : ", fiftyNotes);
+		writeln("20's : ", twentyNotes);
+		writeln("10's : ", tenNotes);
+	}
+
 	// other member functions ...
 }
 
 import std.stdio;
 import std.c.stdlib; 
+import std.exception;
 
 int main(string[] argv)
 {
 	writeln("---------------------------------------------------------");
 	writeln("| Demostrating Design by Contract - ATM withdrawal model |");
 	writeln("| Created by Daniel G. Holmes, 2015                      |");
-	writeln("| PLEASE REFER TO README                                 |");
+	writeln("| PLEASE REFER TO THE README                             |");
 	writeln("---------------------------------------------------------");
 	write("\n");
 	system("PAUSE");
 	
 	writeln("Creating a new advancedATM object...");
-	writeln("This models the commishoning of a new ATM.");
-	writeln("Constructor arguements are the initial number of individual notes loaded.");
+	writeln("This models the commissioning of a new ATM.");
+	writeln("Constructor arguments are the initial number of individual notes loaded.");
 
-	auto advancedATM = new AdvancedATM(10, 10, 10, 10, 10);
+	auto advancedATM = new AdvancedATM(100, 200, 300, 400, 500);
 	system("PAUSE");
-	
-	writeln("\n", "bla!");
+	writeln("");
 
+	int withdrawAmount = 0;
+	do
+	{
+		writeln("Please enter a withdrawal amount as an integer multiple of 50 (max 5000).");
+		readf(" %s", &withdrawAmount);
+	}
+	while (withdrawAmount != 0 && withdrawAmount % 50 != 0);
+	// NOTE: These verifications are here to facilitate correct running of the demonstration and
+	//		 are not intended to supersede the Design by Contract precondition assertions.
+	advancedATM.withdraw(withdrawAmount);
+	withdrawAmount = 0;
+	system("PAUSE");
+	writeln("");
+
+	do
+	{
+		writeln("Now enter a withdrawal amount as a multiple of 10, but not 50 (max 5000).");
+		readf(" %s", &withdrawAmount);
+	}
+	while (withdrawAmount != 0 && withdrawAmount % 10 != 0 && withdrawAmount % 50 != 0);
+	// NOTE: These verifications are here to facilitate correct running of the demonstration and
+	//		 are not intended to supersede the Design by Contract precondition assertions.
+	advancedATM.withdraw(withdrawAmount);
+	withdrawAmount = 0;
+	system("PAUSE");
+	writeln("");
+	
+	writeln("Demonstration complete!");
+	writeln("Enter an invalid amount to see a failed preconditions exception in derived class.");
+	readf(" %s", &withdrawAmount);
+	advancedATM.withdraw(withdrawAmount);
 	system("PAUSE");
 	
     return 0;
